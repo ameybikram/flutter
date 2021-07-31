3c7a7e025881180cfda34168ee751b2cb03d3476
Amey <a.bikram492@gmail.com>
2021-08-01 02:27:10 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,12 +20,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.latoTextTheme()
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark, // dark color brightness will override red
         primarySwatch: Colors.red,
       ),
-      initialRoute: "/home",
+      // initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(), // meaning home routes
         "/home": (context) => HomePage(), // object of class HomePage
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,19 +6,66 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20, // fontSize shouldn't be explicitally specified
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/login_image.png',
+            fit: BoxFit.cover,
           ),
-
-        ),
+          SizedBox(
+            // for giving space between two childs
+            height: 20.0,
+            // child: Text('Space'),
+          ),
+          Text(
+            'Welcome',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            // for giving space between two childs
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Username',
+                    labelText: 'UserName',
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true, // To hide the password
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password',
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text('Login'),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print('Hi Amey');
+                  },
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
 }
 
-// hit ctrl + space + down arrow button for hints
+// hit ctrl + space button for hints
+// main axis of row is horizontal axis and cross axis is vertical axis whereas for columns, main axis is vertical axis and cross axis is horizontal axis
+// There are two types of fields => form field and text field
