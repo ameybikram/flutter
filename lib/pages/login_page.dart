513440f42791d1739e092ccf81bd50419663772c
Amey <a.bikram492@gmail.com>
2021-08-02 03:37:44 +0530

    day6 commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key); // constructor
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -23,7 +29,7 @@ class LoginPage extends StatelessWidget {
               // child: Text('Space'),
             ),
             Text(
-              'Welcome',
+              'Welcome $name',
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
@@ -43,6 +49,11 @@ class LoginPage extends StatelessWidget {
                       hintText: 'Enter Username',
                       labelText: 'UserName',
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(
+                          () {}); //To call the build methd again and can be used in stateful widget
+                    },
                   ),
                   TextFormField(
                     obscureText: true, // To hide the password
@@ -54,14 +65,50 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                    child: Text('Login'),
-                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    onPressed: () {
-                      // print('Hi Amey');
+                  InkWell(
+                    onTap: () async {
+                      // Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      // There is no onPressed button in Container as it was in case of ElevatedButton hence there will be no clickable button. So to make any widget clickable we can wrap the unclickable widget by using gesturedetector or inkwell(recommended).
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text('Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              )),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // shape: changeButton
+                        //     ? BoxShape.circle
+                        //     : BoxShape.rectangle,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 0),
+                      ),
+                    ),
                   )
+                  // ElevatedButton(
+                  //   child: Text('Login'),
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  //   onPressed: () {
+                  //     // print('Hi Amey');
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
                 ],
               ),
             )
